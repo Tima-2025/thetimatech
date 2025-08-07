dfde7e12782c21e206a4e1caa448a61d1a7b1c8e
Timatech <timaintegrated@gmail.com>
2025-08-07 07:48:47 -0700

    Update TIMAWebsite.tsx

--- src/components/TIMAWebsite.tsx
+++ src/components/TIMAWebsite.tsx
@@ -1189,8 +1189,8 @@ const TIMAWebsite = () => {
           preload="auto"
           className="absolute inset-0 w-full h-full object-cover opacity-40"
         >
-          <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" type="video/mp4" />
-          <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" type="video/mp4" />
+          <source src="https://videos.pexels.com/video-files/7568747/7568747-hd_1920_1080_25fps.mp4" type="video/mp4" />
+          <source src="https://videos.pexels.com/video-files/7568747/7568747-hd_1920_1080_25fps.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/20 to-black/60"></div>
         
